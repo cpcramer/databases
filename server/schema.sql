323e654234694d0b4afad5f20e998f01a0ddfd19
Chadcramer <chadpcramer@gmail.com>
2019-05-02 11:43:09 -0500

    database schema implemented

--- server/schema.sql
+++ server/schema.sql
@@ -2,18 +2,81 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-    id INT NOT NULL AUTO_INCREMENT, 
-    owner VARCHAR(20),
-    contents VARCHAR(20),
-    PRIMARY KEY(id)  
-);
 
 /* Create other tables and define schemas for them here! */
+-- ---
+-- Globals
+-- ---
 
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
 
+-- ---
+-- Table 'messages'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS `messages`;
+		
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user` INTEGER NOT NULL DEFAULT -1,
+  `room` INTEGER NOT NULL DEFAULT -1,
+  `text` VARCHAR(300) NOT NULL DEFAULT '""',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+		
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user` VARCHAR(20) NOT NULL DEFAULT '""',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+		
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `room` VARCHAR(20) NOT NULL DEFAULT '""',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (user) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`user`,`room`,`text`) VALUES
+-- ('','','','');
+-- INSERT INTO `users` (`id`,`user`) VALUES
+-- ('','');
+-- INSERT INTO `rooms` (`id`,`room`) VALUES
+-- ('','');
 
 /*  Execute this file from the command line by typing:
  *    mysql -u student < server/schema.sql
